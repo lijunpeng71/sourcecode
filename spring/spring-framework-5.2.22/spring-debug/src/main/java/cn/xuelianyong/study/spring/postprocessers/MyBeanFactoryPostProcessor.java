c5d77ac25b6f6392e09c47f30ec4677ae4b42ba1
lijunpeng71 <lijunpeng71@163.com>
2022-11-29 22:26:33 +0800

    init

---
+++ spring/spring-framework-5.2.22/spring-debug/src/main/java/cn/xuelianyong/study/spring/postprocessers/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,16 @@
+package cn.xuelianyong.study.spring.postprocessers;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * @author lijunpeng
+ * 2022/11/29 21:29
+ */
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		System.out.println("MyBeanFactoryPostProcessor");
+	}
+}
